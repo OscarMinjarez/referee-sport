72a3b00779f9ba8d03b0e2827400cbea362b7c1e
OscarMinjarez <minjarez.dev@gmail.com>
2025-03-28 10:01:56 -0700

    Se movieron apps y se inicializo proyecto general con npm

--- backend/apps/employees/src/main.ts
+++ backend/apps/employees/src/main.ts
@@ -3,6 +3,6 @@ import { EmployeesModule } from './employees.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(EmployeesModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
